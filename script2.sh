616048fb54ecc48db05ca39804b08b371d0baaea
Vedansh24BCE10394 <vedansh.24bce10394@vitbhopal.ac.in>
2026-03-31 19:47:41 +0530

    Add files via upload

---
+++ script2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script 2: FOSS Package Inspector
+# Author: Vedansh
+
+PACKAGE="git"
+
+# Check if package is installed
+if dpkg -l | grep -q $PACKAGE; then
+ echo "$PACKAGE is installed."
+ dpkg -l | grep $PACKAGE
+else
+ echo "$PACKAGE is NOT installed."
+fi
+
+# Case statement
+case $PACKAGE in
+ git) echo "Git: distributed version control system used worldwide" ;;
+ vlc) echo "VLC: open source media player" ;;
+ firefox) echo "Firefox: open web browser" ;;
+ apache2) echo "Apache: web server software" ;;
+ *) echo "Unknown package" ;;
+esac
